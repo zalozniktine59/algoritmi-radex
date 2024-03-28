a3781478e104314e5c99b2e52194b0516361b24b
zalozniktine <tine.zaloznik@student.um.si>
2024-03-28 21:22:27 +0100

    Dodano decimal to binary

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void numToBinary(int number, std::vector<int>& binaryNumberVector){
+    binaryNumberVector.clear();
+    for (int i=0; i<8; i++) {
+        binaryNumberVector.push_back(number & 1);
+        number >>= 1;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> binary;
+    numToBinary(10, binary);
+    for (int i=binary.size()-1; i>=0; i--) {
+        std::cout << binary[i];
+    }
+    cout<<endl;
     return 0;
 }
